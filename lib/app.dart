bb76c14cb07e8a7b6cbeb4a9b0a9a52b6a7d48a3
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-01 20:50:54 +0900

    Extract TodoListPage component

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'domain/todo_list_service.dart';
+import 'pages/todo_list_page.dart';
 
 class App extends StatelessWidget {
   final TodoListService _service;
@@ -13,77 +14,5 @@ class App extends StatelessWidget {
       title: 'Fun To Do List',
       home: TodoListPage(_service),
     );
-  }
-}
-
-class TodoListPage extends StatefulWidget {
-  final TodoListService _service;
-
-  const TodoListPage(this._service, {super.key});
-
-  @override
-  State<TodoListPage> createState() => _TodoListPageState();
-}
-
-class _TodoListPageState extends State<TodoListPage> {
-  String _title = '';
-  List<Todo> _todos = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _todos = widget._service.listTodos();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView(
-          children: [
-            for (final todo in _todos)
-              ListTile(
-                title: Text(todo.title),
-              ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Container(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          const Text('New Todo',
-                              style: TextStyle(fontSize: 24)),
-                          TextField(
-                            autofocus: true,
-                            decoration: const InputDecoration(
-                              labelText: 'Title',
-                            ),
-                            onChanged: (value) {
-                              setState(() {
-                                _title = value;
-                              });
-                            },
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              widget._service.addTodo(_title);
-                              Navigator.pop(context);
-                              setState(() {
-                                _todos = widget._service.listTodos();
-                              });
-                            },
-                            child: const Text('Add'),
-                          )
-                        ],
-                      ));
-                });
-          },
-          tooltip: 'Add Todo',
-          child: const Icon(Icons.add),
-        ));
   }
 }
---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import 'package:fun_todo_list/domain/todo_list_service.dart';
+
+class TodoListPage extends StatefulWidget {
+  final TodoListService _service;
+
+  const TodoListPage(this._service, {super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  String _title = '';
+  List<Todo> _todos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _todos = widget._service.listTodos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+          children: [
+            for (final todo in _todos)
+              ListTile(
+                title: Text(todo.title),
+              ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          const Text('New Todo',
+                              style: TextStyle(fontSize: 24)),
+                          TextField(
+                            autofocus: true,
+                            decoration: const InputDecoration(
+                              labelText: 'Title',
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                _title = value;
+                              });
+                            },
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              widget._service.addTodo(_title);
+                              Navigator.pop(context);
+                              setState(() {
+                                _todos = widget._service.listTodos();
+                              });
+                            },
+                            child: const Text('Add'),
+                          )
+                        ],
+                      ));
+                });
+          },
+          tooltip: 'Add Todo',
+          child: const Icon(Icons.add),
+        ));
+  }
+}
