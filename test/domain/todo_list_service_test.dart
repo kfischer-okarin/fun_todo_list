44b41093366033888264e27abe80d28e14fdaf9f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-02 13:30:11 +0900

    Refactor service test setup

--- test/domain/todo_list_service_test.dart
+++ test/domain/todo_list_service_test.dart
@@ -3,13 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fun_todo_list/domain/todo_list_service.dart';
 
 void main() {
+  TodoListService buildService() => TodoListService();
+
   setUp(() {
     TodoListService.todos.clear();
   });
 
   group('addTodo', () {
     test('return added todo', () {
-      final service = TodoListService();
+      final service = buildService();
 
       final todo = service.addTodo('Buy milk');
 
@@ -19,7 +21,7 @@ void main() {
 
   group('listTodos', () {
     test('return all added todos', () {
-      final service = TodoListService();
+      final service = buildService();
       service.addTodo('Buy milk');
       service.addTodo('Buy eggs');
 
